97b9c70e5442b0f9dab8a2fb3f13b4bd342e82ff
Maksim Syrnev <msyrnev@yandex.ru>
2021-10-20 00:48:20 +1000

    bugfix

--- src/services/middleware/socketMiddleware.js
+++ src/services/middleware/socketMiddleware.js
@@ -1,3 +1,13 @@
+import {
+  WS_CONNECTION_START,
+  WS_CONNECTION_SUCCESS,
+  WS_CONNECTION_ERROR,
+  WS_CONNECTION_CLOSED,
+  WS_GET_MESSAGE,
+  WS_CONNECTION_CLOSE,
+  WS_SEND_MESSAGE
+} from '../actions/ws-action-type';
+
 export const socketMiddleware = () => {
   return store => {
     let socket = null;
@@ -6,21 +16,21 @@ export const socketMiddleware = () => {
       const { dispatch, getState } = store;
       const { type, payload } = action;
 
-      if (type === 'WS_CONNECTION_START') {
+      if (type === WS_CONNECTION_START) {
         socket = new WebSocket(payload);
       }
 
       if (socket) {
         socket.onopen = event => {
-          dispatch({ type: 'WS_CONNECTION_SUCCESS', payload: event });
+          dispatch({ type: WS_CONNECTION_SUCCESS, payload: event });
         };
 
         socket.onerror = event => {
-          dispatch({ type: 'WS_CONNECTION_ERROR', payload: event });
+          dispatch({ type: WS_CONNECTION_ERROR, payload: event });
         };
 
         socket.onclose = event => {
-          dispatch({ type: 'WS_CONNECTION_CLOSED', payload: event });
+          dispatch({ type: WS_CONNECTION_CLOSED, payload: event });
         };
 
         socket.onmessage = event => {
@@ -28,15 +38,15 @@ export const socketMiddleware = () => {
           const parsedData = JSON.parse(data);
           const { success, ...restParsedData } = parsedData;
           if (success) {
-            dispatch({ type: 'WS_GET_MESSAGE', payload: restParsedData });
+            dispatch({ type: WS_GET_MESSAGE, payload: restParsedData });
           }
         };
 
-        if (type === 'WS_CONNECTION_CLOSE') {
+        if (type === WS_CONNECTION_CLOSE) {
           socket.close();
         }
 
-        if (type === 'WS_SEND_MESSAGE') {
+        if (type === WS_SEND_MESSAGE) {
           const message = payload;
           socket.send(JSON.stringify(message));
         }
